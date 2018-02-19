c3832dd8e3f44f8b9f09c0c0e86f0dfa6d98a0f4
Vivialyn Evasco <36612264+bubalynnn@users.noreply.github.com>
2018-02-19 04:48:41 +0000

    Add files via upload

---
+++ EVASCO Exercise 2.py
@@ -0,0 +1,6 @@
+x=input ("Input number:")
+fact=1
+for x in range (1,x+1):
+    fact=fact*x
+print"The factorial value is", fact
+    
---
+++ EVASCO Exercise1.py
@@ -0,0 +1,5 @@
+money =input("How much money is in your account?")
+print"You have",money,"on your account."
+iphone=50000
+print"You can afford", money/iphone,"unit/s of iPhone X."
+print"You have",money-iphone,"left on your account."
